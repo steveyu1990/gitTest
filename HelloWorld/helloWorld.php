b84e3ba9427b0a9ad14cc3d7d95b76ebd259bd07
steveyu <837314446@qq.com>
2020-03-21 23:06:34 +0800

    增加测试

--- HelloWorld/helloWorld.php
+++ HelloWorld/helloWorld.php
@@ -1,2 +1,36 @@
 <?php
-echo 'hello world.we are one, branch 1';
+$a = [
+    [1,10,3 ,8],
+    [12,2,9,6],
+    [5,7,4,11],
+    [3,7,16,5]
+];
+
+function getMax($a) {
+    if (empty($a)) {
+        return null;
+    }
+
+    $maxMatrix = [];
+    $rows = count($a);
+    $cols = count($a[0]);
+
+    for ($i=0; $i<$rows; $i++) {
+        for ($j=0; $j<$cols; $j++) {
+            $maxMatrix[$i][$j] = 0;
+        }
+    }
+
+    for ($i=0; $i<$rows; $i++) {
+        for ($j=0; $j<$cols; $j++) {
+            $left = $maxMatrix[$i-1][$j] ?? 0;
+            $up = $maxMatrix[$i][$j-1] ?? 0;
+            $maxMatrix[$i][$j] = max($left, $up) + $a[$i][$j];
+        }
+    }
+
+    return $maxMatrix[$rows-1][$cols-1];
+}
+
+$result = getMax($a);
+echo $result;
